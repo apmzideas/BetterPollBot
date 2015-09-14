efbcf1dae40499ef202d70a68a86cebf38b210f8
TheRedFireFox <adrian.hornung@yahoo.de>
2015-09-14 16:40:01 +0200

    small fix

--- Py2Pot.bat
+++ Py2Pot.bat
@@ -2,5 +2,5 @@
 REM cd C:\Users\Adrian\Documents\GitHub\BetterPollBot
 
 
-py -3.4  C:\PYthon34\Tools\i18n\pygettext.py -n --keyword=_ --default-domain=Telegram --output-dir=translation_template src/*.py src/language/*.py src/messages/*.py src/parsers/*.py src/sql/*.py
+py -3.4  C:\PYthon34\Tools\i18n\pygettext.py -n --keyword=_ --default-domain=Telegram --output-dir=translation src/*.py src/language/*.py src/messages/*.py src/parsers/*.py src/sql/*.py
 pause
